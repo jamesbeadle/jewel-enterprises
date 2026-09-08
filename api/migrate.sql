cc6c1478e8d8b1eb541f1ad30b8fe7cacaacc57e
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-09-08 12:20:49 +0100

    add migration

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,131 +3,42 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908120000_AddWorkerToXeroLineTimesheetCovers'
+    WHERE [MigrationId] = N'20260908180000_AddWorkOrderBillApprovals'
 )
 BEGIN
-    ALTER TABLE [XeroLineTimesheetCovers] ADD [WorkerId] nvarchar(64) NULL;
+    CREATE TABLE [WorkOrderBillApprovals] (
+        [WorkOrderBillApprovalId] nvarchar(64) NOT NULL,
+        [XeroInvoiceId] nvarchar(64) NOT NULL,
+        [WorkOrderId] nvarchar(64) NOT NULL,
+        [ProjectId] nvarchar(64) NOT NULL,
+        [MatchRule] int NOT NULL,
+        [MatchDetail] nvarchar(512) NOT NULL,
+        [BillNet] decimal(18,4) NOT NULL,
+        [ApprovedByEmail] nvarchar(256) NOT NULL,
+        [ApprovedAtUtc] datetimeoffset NOT NULL,
+        [UndoneByEmail] nvarchar(256) NULL,
+        [UndoneAtUtc] datetimeoffset NULL,
+        CONSTRAINT [PK_WorkOrderBillApprovals] PRIMARY KEY ([WorkOrderBillApprovalId])
+    );
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908120000_AddWorkerToXeroLineTimesheetCovers'
+    WHERE [MigrationId] = N'20260908180000_AddWorkOrderBillApprovals'
+)
+BEGIN
+    CREATE INDEX [IX_WorkOrderBillApprovals_XeroInvoiceId] ON [WorkOrderBillApprovals] ([XeroInvoiceId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260908180000_AddWorkOrderBillApprovals'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260908120000_AddWorkerToXeroLineTimesheetCovers', N'8.0.10');
-END;
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE TABLE [ProgrammeTaskCostCentres] (
-        [ProgrammeTaskCostCentreId] nvarchar(64) NOT NULL,
-        [ProjectId] nvarchar(64) NOT NULL,
-        [ProgrammeTaskId] nvarchar(64) NOT NULL,
-        [CostCode] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK_ProgrammeTaskCostCentres] PRIMARY KEY ([ProgrammeTaskCostCentreId])
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE INDEX [IX_ProgrammeTaskCostCentres_ProjectId] ON [ProgrammeTaskCostCentres] ([ProjectId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE INDEX [IX_ProgrammeTaskCostCentres_ProgrammeTaskId] ON [ProgrammeTaskCostCentres] ([ProgrammeTaskId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE TABLE [ProgrammeDrafts] (
-        [ProgrammeDraftId] nvarchar(64) NOT NULL,
-        [ProjectId] nvarchar(64) NOT NULL,
-        [ValuationClaimId] nvarchar(64) NOT NULL,
-        [ClaimName] nvarchar(128) NOT NULL,
-        [Status] int NOT NULL,
-        [CreatedAt] datetimeoffset NOT NULL,
-        [CreatedByEmail] nvarchar(256) NOT NULL,
-        [ResolvedAt] datetimeoffset NULL,
-        [ResolvedByEmail] nvarchar(256) NOT NULL,
-        [SuggestionsRequestedAt] datetimeoffset NULL,
-        [SuggestionsNote] nvarchar(512) NOT NULL,
-        CONSTRAINT [PK_ProgrammeDrafts] PRIMARY KEY ([ProgrammeDraftId])
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE INDEX [IX_ProgrammeDrafts_ProjectId_Status] ON [ProgrammeDrafts] ([ProjectId], [Status]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE TABLE [ProgrammeDraftLines] (
-        [ProgrammeDraftLineId] nvarchar(64) NOT NULL,
-        [ProgrammeDraftId] nvarchar(64) NOT NULL,
-        [ProgrammeTaskId] nvarchar(64) NOT NULL,
-        [TaskTitle] nvarchar(256) NOT NULL,
-        [CurrentPercent] decimal(18,4) NOT NULL,
-        [ProposedPercent] decimal(18,4) NULL,
-        [CostCodes] nvarchar(512) NOT NULL,
-        [MappingSource] int NOT NULL,
-        [Evidence] nvarchar(1024) NOT NULL,
-        [IsIncluded] bit NOT NULL,
-        [ReviewedPercent] decimal(18,4) NULL,
-        CONSTRAINT [PK_ProgrammeDraftLines] PRIMARY KEY ([ProgrammeDraftLineId])
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    CREATE INDEX [IX_ProgrammeDraftLines_ProgrammeDraftId] ON [ProgrammeDraftLines] ([ProgrammeDraftId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260908150000_AddProgrammeDrafts'
-)
-BEGIN
-    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260908150000_AddProgrammeDrafts', N'8.0.10');
+    VALUES (N'20260908180000_AddWorkOrderBillApprovals', N'8.0.10');
 END;
 GO
 
