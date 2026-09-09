d42ba64a57f9c2a08742f3a70e8485648dce9dba
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-09-09 15:46:05 +0100

    Add migration log

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,66 +3,28 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
+    WHERE [MigrationId] = N'20260910100000_AddDocumentOcrResults'
 )
 BEGIN
-    CREATE TABLE [ProgrammeVariationEffects] (
-        [ProgrammeVariationEffectId] nvarchar(64) NOT NULL,
-        [ProjectId] nvarchar(64) NOT NULL,
-        [VariationOrderId] nvarchar(64) NOT NULL,
-        [ProgrammeTaskId] nvarchar(64) NOT NULL,
-        [DelayDays] int NOT NULL,
-        [Note] nvarchar(512) NOT NULL,
-        [RecordedByEmail] nvarchar(256) NOT NULL,
-        [RecordedAt] datetimeoffset NOT NULL,
-        CONSTRAINT [PK_ProgrammeVariationEffects] PRIMARY KEY ([ProgrammeVariationEffectId])
+    CREATE TABLE [DocumentOcrResults] (
+        [ContentSha256] nvarchar(64) NOT NULL,
+        [PageCount] int NOT NULL,
+        [PagesJson] nvarchar(max) NOT NULL,
+        [Confidence] float NOT NULL,
+        [Provider] nvarchar(64) NOT NULL,
+        [CreatedAtUtc] datetimeoffset NOT NULL,
+        CONSTRAINT [PK_DocumentOcrResults] PRIMARY KEY ([ContentSha256])
     );
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
-)
-BEGIN
-    CREATE INDEX [IX_ProgrammeVariationEffects_ProjectId] ON [ProgrammeVariationEffects] ([ProjectId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
-)
-BEGIN
-    EXEC(N'CREATE UNIQUE INDEX [IX_ProgrammeVariationEffects_VariationOrderId_ProgrammeTaskId] ON [ProgrammeVariationEffects] ([VariationOrderId], [ProgrammeTaskId]) WHERE [VariationOrderId] IS NOT NULL AND [ProgrammeTaskId] IS NOT NULL');
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
-)
-BEGIN
-    ALTER TABLE [Requests] ADD [EotDaysClaimed] int NULL;
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
-)
-BEGIN
-    ALTER TABLE [Requests] ADD [EotDaysGranted] int NULL;
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909160000_AddProgrammeVariationEffects'
+    WHERE [MigrationId] = N'20260910100000_AddDocumentOcrResults'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260909160000_AddProgrammeVariationEffects', N'8.0.10');
+    VALUES (N'20260910100000_AddDocumentOcrResults', N'8.0.10');
 END;
 GO
 
