98c4ce79968446eb5acc4e07e62fdf1f0e52be4d
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-09-09 13:41:39 +0100

    add migration

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,26 +3,38 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909110000_DropCompanyContactPurpose'
+    WHERE [MigrationId] = N'20260909150000_AddXeroLineWorkOrderLinkCostCenterCode'
 )
 BEGIN
-    DECLARE @var0 sysname;
-    SELECT @var0 = [d].[name]
-    FROM [sys].[default_constraints] [d]
-    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
-    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[CompanyContacts]') AND [c].[name] = N'Purpose');
-    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [CompanyContacts] DROP CONSTRAINT [' + @var0 + '];');
-    ALTER TABLE [CompanyContacts] DROP COLUMN [Purpose];
+    ALTER TABLE [XeroLineWorkOrderLinks] ADD [CostCenterCode] nvarchar(32) NULL;
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260909110000_DropCompanyContactPurpose'
+    WHERE [MigrationId] = N'20260909150000_AddXeroLineWorkOrderLinkCostCenterCode'
+)
+BEGIN
+    DROP INDEX [UX_XeroLineWorkOrderLinks_Line_Order] ON [XeroLineWorkOrderLinks];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260909150000_AddXeroLineWorkOrderLinkCostCenterCode'
+)
+BEGIN
+    EXEC(N'CREATE UNIQUE INDEX [UX_XeroLineWorkOrderLinks_Line_Order_Code] ON [XeroLineWorkOrderLinks] ([XeroLedgerLineId], [WorkOrderId], [CostCenterCode]) WHERE [XeroLedgerLineId] IS NOT NULL AND [WorkOrderId] IS NOT NULL AND [CostCenterCode] IS NOT NULL');
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260909150000_AddXeroLineWorkOrderLinkCostCenterCode'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260909110000_DropCompanyContactPurpose', N'8.0.10');
+    VALUES (N'20260909150000_AddXeroLineWorkOrderLinkCostCenterCode', N'8.0.10');
 END;
 GO
 
