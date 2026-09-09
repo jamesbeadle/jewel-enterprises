1b4803973997b6a1954b78947808e071a21e7d20
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-09-09 16:58:49 +0100

    Add migration log

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,28 +3,38 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260910100000_AddDocumentOcrResults'
+    WHERE [MigrationId] = N'20260910110000_AddValuationInvoiceXeroRaise'
 )
 BEGIN
-    CREATE TABLE [DocumentOcrResults] (
-        [ContentSha256] nvarchar(64) NOT NULL,
-        [PageCount] int NOT NULL,
-        [PagesJson] nvarchar(max) NOT NULL,
-        [Confidence] float NOT NULL,
-        [Provider] nvarchar(64) NOT NULL,
-        [CreatedAtUtc] datetimeoffset NOT NULL,
-        CONSTRAINT [PK_DocumentOcrResults] PRIMARY KEY ([ContentSha256])
-    );
+    ALTER TABLE [ValuationInvoices] ADD [XeroInvoiceId] nvarchar(64) NULL;
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260910100000_AddDocumentOcrResults'
+    WHERE [MigrationId] = N'20260910110000_AddValuationInvoiceXeroRaise'
+)
+BEGIN
+    ALTER TABLE [ValuationInvoices] ADD [XeroInvoiceNumber] nvarchar(64) NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260910110000_AddValuationInvoiceXeroRaise'
+)
+BEGIN
+    ALTER TABLE [ValuationInvoices] ADD [XeroRaisedAt] datetimeoffset NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260910110000_AddValuationInvoiceXeroRaise'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260910100000_AddDocumentOcrResults', N'8.0.10');
+    VALUES (N'20260910110000_AddValuationInvoiceXeroRaise', N'8.0.10');
 END;
 GO
 
